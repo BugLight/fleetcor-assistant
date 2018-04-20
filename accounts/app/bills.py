1fc15b84928cd796a8af5b810449f08ea6a82f0e
Buglight <buglight@kistriver.com>
2018-04-20 18:06:21 +0300

    Bill create

--- accounts/app/bills.py
+++ accounts/app/bills.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, abort, jsonify
+from flask import Blueprint, abort, jsonify, request
 from accounts.app import api_info
 from marshmallow_jsonschema import JSONSchema
 
-from accounts.app.models import Bill
+from accounts.app.models import Bill, db
 from accounts.app.schemas import BillSchema
 
 
@@ -20,3 +20,17 @@ def get_bill(id):
     if not bill:
         abort(404)
     return jsonify(bill=bill_schema.dump(bill))
+
+
+@bills.route('', methods=['POST'])
+def create_bill():
+    data = request.get_json()
+    if not data:
+        abort(400)
+    data = bill_schema.load(data)
+    bill = Bill(**data)
+    db.session.add(bill)
+    db.session.commit()
+    return jsonify({
+        'message': 'succeed'
+    }), 201
