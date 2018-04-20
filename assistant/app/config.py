6cd270d3a446100a6fffebe4fde8670b4daf20cb
Buglight <buglight@kistriver.com>
2018-04-21 01:29:38 +0300

    Query parser extension

--- assistant/app/config.py
+++ assistant/app/config.py
@@ -5,3 +5,4 @@ DEBUG = strtobool(os.environ.get('DEBUG', 'yes'))
 SECRET_KEY = os.environ.get('SECRET', 'defaultsecret')
 
 APPLICATION_ROOT = '/api/assistant'
+API_URL = 'http://localhost/api/accounts/'
