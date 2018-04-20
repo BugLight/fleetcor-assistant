16d97166200f44345db7adbbdee38cda5e8bd179
Buglight <buglight@kistriver.com>
2018-04-20 15:21:49 +0300

    Fixed db file

--- accounts/app/config.py
+++ accounts/app/config.py
@@ -4,4 +4,4 @@ from distutils.util import strtobool
 DEBUG = strtobool(os.environ.get('DEBUG', 'yes'))
 SECRET_KEY = os.environ.get('SECRET', 'defaultsecret')
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///./database.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/database.db'
