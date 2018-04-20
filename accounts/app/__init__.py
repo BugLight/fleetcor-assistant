45974faec809e845967ff983e9cbf4ba08581f78
Buglight <buglight@kistriver.com>
2018-04-20 20:45:13 +0300

    Routes prefixer flask extension

--- accounts/app/__init__.py
+++ accounts/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from accounts.app.ext.api_info import ApiInfo
+from accounts.app.ext.routes_prefixer import RoutesPrefixer
 from accounts.app.models import db
 from flask_migrate import Migrate
 from werkzeug.exceptions import HTTPException
@@ -10,22 +11,20 @@ def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
 
+    prefixer = RoutesPrefixer(app, app.config['APPLICATION_ROOT'])
+
     api_info.init_app(app)
     db.init_app(app)
 
     migrate = Migrate(app, db)
 
     from accounts.app.accounts import accounts
-    app.register_blueprint(accounts,
-                           url_prefix=app.config['APPLICATION_ROOT'] + \
-                           '/accounts')
+    app.register_blueprint(accounts, url_prefix='/accounts')
 
     from accounts.app.app_error import process_error
     app.register_error_handler(404, process_error)
 
     from accounts.app.bills import bills
-    app.register_blueprint(bills,
-                           url_prefix=app.config['APPLICATION_ROOT'] + \
-                           '/bills')
+    app.register_blueprint(bills, url_prefix='/bills')
 
     return app
