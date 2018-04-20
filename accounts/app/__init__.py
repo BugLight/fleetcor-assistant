3bdc5bac01e15c678989997d038d37692cf6676b
Buglight <buglight@kistriver.com>
2018-04-20 14:05:49 +0300

    AppInfo extension

--- accounts/app/__init__.py
+++ accounts/app/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from accounts.app.ext.api_info import ApiInfo
+
+api_info = ApiInfo()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+
+    api_info.init_app(app)
 
     return app
