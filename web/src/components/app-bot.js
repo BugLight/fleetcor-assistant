127d2ed3eb9c8d4ee20b4cb36d41c9e7d1d8ecbd
pettro98 <ya.skywalker@yandex.ru>
2018-04-21 05:54:42 +0300

    improved chat dialog appearance

--- web/src/components/app-bot.js
+++ web/src/components/app-bot.js
@@ -1,39 +1,35 @@
 export default {
     render(h) {
-        return (
-            <div class="bot">
+        return <div class="bot">
                 <div class="bot_header">
-                    <input
-                        type="checkbox"
-                        id="hideChat"
-                        onClick={() => {
+                    <input type="checkbox" id="hideChat" onClick={() => {
                             this.$store.commit('toggleBot');
-                        }}
-                    />
+                        }} />
                     <label for="hideChat">
                         <transition name="rotate-arrow">
                             {this.$store.state.botVisible ? (
-                                <img src="/static/images/down_arrow.svg" />
+                                <img
+                                    style="cursor: pointer;"
+                                    src="/static/images/down_arrow.svg"
+                                />
                             ) : (
-                                <img src="/static/images/up_arrow.svg" />
+                                <img
+                                    style="cursor: pointer;"
+                                    src="/static/images/up_arrow.svg"
+                                />
                             )}
                         </transition>
                     </label>
                     <span>Личный помощник</span>
                 </div>
                 <transition name="slide-bot">
-                    {this.$store.state.botVisible ? (
-                        <div class="back_panel">
+                    {this.$store.state.botVisible ? <div class="back_panel">
                             <div class="user_field">
-                                <input
-                                    type="text"
-                                    placeholder="Введите комманду.."
-                                />
+                                <input type="text" placeholder="Введите комманду.." />
+                                <img src="/static/images/send.svg" />
                             </div>
-                        </div>
-                    ) : null}
+                        </div> : null}
                 </transition>
-            </div>
-        );
+            </div>;
     }
 };
